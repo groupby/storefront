bbf942126308a474ebb1263dede84e40fe5ad26b
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2018-12-17 13:06:22 -0500

    Remove console logs

--- packages/@storefront/core/src/index.ts
+++ packages/@storefront/core/src/index.ts
@@ -1,8 +1,6 @@
 import './globals';
 import StoreFront from './storefront';
 
-console.log('__ INSIDE `@storefront/core` `index.ts`'); // TEMP
-
 export * from './core';
 export * from './tags';
 export { Actions, Events, Selectors, Store, StoreSections } from '@storefront/flux-capacitor';
--- packages/@storefront/flux-capacitor/src/index.ts
+++ packages/@storefront/flux-capacitor/src/index.ts
@@ -1,6 +1,4 @@
 import FluxCapacitor from './flux-capacitor';
 
-console.log('__ INSIDE `@storefront/flux-capacitor` `index.ts`'); // TEMP
-
 export * from './core';
 export default FluxCapacitor;
