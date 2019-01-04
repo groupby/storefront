eb6f59b0250b4d9a57a0467786151ac3c910d55c
blevy115 <brandon.levy@groupbyinc.com>
2019-01-04 18:12:54 -0500

    added spaces to unit test

--- packages/@storefront/sayt/test/unit/previous-search-terms.ts
+++ packages/@storefront/sayt/test/unit/previous-search-terms.ts
@@ -25,9 +25,13 @@ suite('PreviousSearchTerms', ({ expect, spy, itShouldProvideAlias }) => {
       })
     })
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 >>>>>>> changed alias name, wrote initial unit test for previous searches component and added file to bootstrap
+=======
+
+>>>>>>> added spaces to unit test
     describe('state', () => {
       describe('previousSearches', () => {
         it('should set inital value', () => {
@@ -37,9 +41,13 @@ suite('PreviousSearchTerms', ({ expect, spy, itShouldProvideAlias }) => {
     })
   })
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 >>>>>>> changed alias name, wrote initial unit test for previous searches component and added file to bootstrap
+=======
+
+>>>>>>> added spaces to unit test
   describe('init()', () => {
     it('should listen for flux events', () => {
       const subscribe = (previousSearchTerms.subscribe = spy());
@@ -49,6 +57,7 @@ suite('PreviousSearchTerms', ({ expect, spy, itShouldProvideAlias }) => {
     })
   })
 <<<<<<< HEAD
+<<<<<<< HEAD
 
   describe('updatePreviousSearches', () => {
     let subscribe, query, handler;
@@ -84,30 +93,38 @@ suite('PreviousSearchTerms', ({ expect, spy, itShouldProvideAlias }) => {
     })
   })
 =======
+=======
+
+>>>>>>> added spaces to unit test
   describe('updatePreviousSearches', () => {
 <<<<<<< HEAD
 <<<<<<< HEAD
     let subscribe, query, handler;
+
     beforeEach(() => {
       subscribe = (previousSearchTerms.subscribe = spy());
       previousSearchTerms.init()
       query = 'diamond rings';
       handler = previousSearchTerms.updatePreviousSearches;
     })
+
     it('should add search term if not in array and array length less than limit', () => {
       handler(query);
       expect(previousSearchTerms.state.previousSearches).to.eql(['diamond rings'])
     })
+
     it('should not add the term if it already exist in the array', () => {
       previousSearchTerms.state.previousSearches = ['diamond rings', 'saphire', 'rings', 'diamond', ]
       handler(query);
       expect(previousSearchTerms.state.previousSearches).to.eql(['diamond rings', 'saphire', 'rings', 'diamond'])
     })
+
     it('should remove first term and add new search term if array at limit', () => {
       previousSearchTerms.state.previousSearches = ['saphire', 'rings', 'opals', 'necklaces', 'bracelet', 'earrings'];
       handler(query);
       expect(previousSearchTerms.state.previousSearches).to.eql(['rings', 'opals', 'necklaces', 'bracelet', 'earrings', 'diamond rings'])
     })
+
     it('should move search term to beginning of list if in list and searched again', () => {
       previousSearchTerms.state.previousSearches = ['saphire', 'rings', 'opals', 'necklaces', 'diamond rings', 'earrings'];
       handler(query);
@@ -147,6 +164,9 @@ suite('PreviousSearchTerms', ({ expect, spy, itShouldProvideAlias }) => {
     })
 >>>>>>> wrote more unit test for updatePreviousSearches and added new functinality to move search term to begining if already in the list
   })
+<<<<<<< HEAD
 
 >>>>>>> changed alias name, wrote initial unit test for previous searches component and added file to bootstrap
+=======
+>>>>>>> added spaces to unit test
 })
