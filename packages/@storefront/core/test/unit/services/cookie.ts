27f4c014d451c5d21c8e17b50e04bcd0f79ddebb
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-02-06 12:17:49 -0500

    Update CookieService: add get()

--- packages/@storefront/core/test/unit/services/cookie.ts
+++ packages/@storefront/core/test/unit/services/cookie.ts
@@ -1,3 +1,4 @@
+import * as Cookies from 'js-cookie';
 import Service from '../../../src/services/cookie';
 import StoreFront from '../../../src/storefront';
 import suite from './_suite';
@@ -20,6 +21,22 @@ suite('Cookie Service', ({ expect, spy, itShouldExtendBaseService, stub }) => {
   describe('init()', () => {
     it('should be a no-op', () => {
       expect(() => service.init()).to.not.throw();
+    });
+  });
+
+  describe('get()', () => {
+    let get;
+
+    beforeEach(() => {
+      get = stub(Cookies, 'get');
+    });
+
+    it('should pass through to Cookies.get()', () => {
+      const key = 'foo';
+
+      service.get(key);
+
+      expect(get).to.be.calledWithExactly(key);
     });
   });
 });
