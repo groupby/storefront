ebd4a1775232eea30d6a1a24f67448d1268fd7c3
carloferrer <thecarloferrer@gmail.com>
2019-02-11 16:35:37 -0500

    Force update of infinite-list upon mount
    This is a workaround for the following riot.js issue: https://github.com/riot/riot/issues/2655

--- packages/@storefront/structure/src/infinite-list/index.ts
+++ packages/@storefront/structure/src/infinite-list/index.ts
@@ -3,7 +3,14 @@ import List from '../list';
 
 @provide('infiniteList', (props) => props)
 @tag('gb-infinite-list', require('./index.html'), require('./index.css'))
-class InfiniteList extends List {}
+class InfiniteList extends List {
+  onMount() {
+    // the following is a hotfix to a known riot.js bug; this allows `opts._props` to be parsed as `props`
+    // the riot.js bug is detailed here: https://github.com/riot/riot/issues/2655
+    // todo: Remove this if a riot.js maintainer resolves the issue, or if StoreFront transtions to the React framework.
+    this.set(true);
+  }
+}
 
 namespace InfiniteList {
   export interface Props extends List.Props {
--- packages/@storefront/structure/test/unit/infinite-list.ts
+++ packages/@storefront/structure/test/unit/infinite-list.ts
@@ -15,4 +15,17 @@ suite('InfiniteList', ({ expect, spy, itShouldProvideAlias }) => {
       expect(infiniteList).to.be.an.instanceOf(List);
     });
   });
+
+  // this test corresponds to a hotfix to a known riot.js bug; this allows `opts._props` to be parsed as `props`
+  // the riot.js bug is detailed here: https://github.com/riot/riot/issues/2655
+  // todo: Remove this if a riot.js maintainer resolves the issue, or if StoreFront transtions to the React framework.
+  describe('onMount()', () => {
+    it('should call this.set() with `true`', () => {
+      const set = infiniteList.set = spy();
+
+      infiniteList.onMount();
+
+      expect(set).to.be.calledWith(true);
+    });
+  });
 });
