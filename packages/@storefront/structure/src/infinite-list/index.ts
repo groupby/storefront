567bf743e24584942643dfcb0feba4af6e00138c
carloferrer <thecarloferrer@gmail.com>
2019-02-12 11:08:06 -0500

    Appease linter

--- packages/@storefront/structure/src/infinite-list/index.ts
+++ packages/@storefront/structure/src/infinite-list/index.ts
@@ -8,7 +8,8 @@ class InfiniteList extends List {
     // tslint:disable comment-format
     // XXX: The following is a hotfix to a known riot.js bug; this allows `opts._props` to be parsed as `props`.
     // Source: https://github.com/riot/riot/issues/2655
-    // TODO: Remove this if a riot.js maintainer resolves the issue, or if StoreFront transitions to the React framework.
+    // TODO: Remove this if a riot.js maintainer resolves the issue,
+    // or if StoreFront transitions to the React framework.
     this.set(true);
   }
 }
