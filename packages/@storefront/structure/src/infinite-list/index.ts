a5f5c4037fb17ec6db1cae72745af9991a06dbb5
Carlo Ferrer <thecarloferrer@gmail.com>
2019-02-12 12:19:29 -0500

    SF-1257: Force update on mount of `infinite-list` (#73)
    Force an update on mount of `gb-infinite-list` to properly receive and parse props from its parent, `gb-infinite-scroll`.  This is a workaround for the following issue: https://github.com/riot/riot/issues/2655

--- packages/@storefront/structure/src/infinite-list/index.ts
+++ packages/@storefront/structure/src/infinite-list/index.ts
@@ -3,7 +3,16 @@ import List from '../list';
 
 @provide('infiniteList', (props) => props)
 @tag('gb-infinite-list', require('./index.html'), require('./index.css'))
-class InfiniteList extends List {}
+class InfiniteList extends List {
+  onMount() {
+    // tslint:disable comment-format
+    // XXX: The following is a hotfix to a known riot.js bug; this allows `opts._props` to be parsed as `props`.
+    // Source: https://github.com/riot/riot/issues/2655
+    // TODO: Remove this if a riot.js maintainer resolves the issue,
+    // or if StoreFront transitions to the React framework.
+    this.set(true);
+  }
+}
 
 namespace InfiniteList {
   export interface Props extends List.Props {
--- packages/@storefront/structure/test/unit/infinite-list.ts
+++ packages/@storefront/structure/test/unit/infinite-list.ts
@@ -15,4 +15,18 @@ suite('InfiniteList', ({ expect, spy, itShouldProvideAlias }) => {
       expect(infiniteList).to.be.an.instanceOf(List);
     });
   });
+
+  describe('onMount()', () => {
+  // tslint:disable comment-format
+  // XXX: This test corresponds to a hotfix to a known riot.js bug; this allows `opts._props` to be parsed as `props`.
+  // Source: https://github.com/riot/riot/issues/2655
+  // TODO: Remove this if a riot.js maintainer resolves the issue, or if StoreFront transitions to the React framework.
+    it('should call this.set() with `true`', () => {
+      const set = infiniteList.set = spy();
+
+      infiniteList.onMount();
+
+      expect(set).to.be.calledWith(true);
+    });
+  });
 });
