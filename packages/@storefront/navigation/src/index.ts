b77761a3490c410741ddf0cf46c2ee030d60af8d
Victoria Johns <vmichellej@gmail.com>
2019-02-19 18:31:00 -0500

    Remove filtered-refinement-list and refinement-list

--- packages/@storefront/navigation/src/index.ts
+++ packages/@storefront/navigation/src/index.ts
@@ -1,23 +1,19 @@
 import FilterRefinementControls from './filter-refinement-controls';
-import FilteredRefinementList from './filtered-refinement-list';
 import Navigation from './navigation';
 import NavigationDisplay from './navigation-display';
 import RangeRefinementControls from './range-refinement-controls';
 import './range-selector';
 import Refinement from './refinement';
-import RefinementList from './refinement-list';
 import './refinement-pill';
 import SliderRefinementControls from './slider-refinement-controls';
 import ValueRefinementControls from './value-refinement-controls';
 
 export {
   FilterRefinementControls,
-  FilteredRefinementList,
   Navigation,
   NavigationDisplay,
   SliderRefinementControls,
   RangeRefinementControls,
   Refinement,
-  RefinementList,
   ValueRefinementControls
 };
