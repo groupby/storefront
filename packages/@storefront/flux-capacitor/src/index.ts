65b442fe5b74be052bc1614275dc5684ee6e353f
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-02-26 09:27:14 -0500

    WIP. Add log statements in manifest files

--- packages/@storefront/flux-capacitor/src/index.ts
+++ packages/@storefront/flux-capacitor/src/index.ts
@@ -1,5 +1,7 @@
 import FluxCapacitor from './flux-capacitor';
 
 export { Build, Parse, STOREFRONT_APP_ID } from './flux-capacitor';
+console.log('__ INSIDE `flux-capacitor` MANIFEST');
+
 export * from './core';
 export default FluxCapacitor;
