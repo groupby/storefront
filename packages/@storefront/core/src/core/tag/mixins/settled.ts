4cde4a4f114ae3b4691ca2e0796318e2b3f05c54
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-03-11 16:13:51 -0400

    Defer 'settled' event when application is fetching

--- packages/@storefront/core/src/core/tag/mixins/settled.ts
+++ packages/@storefront/core/src/core/tag/mixins/settled.ts
@@ -17,13 +17,27 @@ let inProgress = 0;
 let timeout;
 
 export default function settledMixin(this: Tag) {
+  const initTimeout = () => {
+    return timeout = setTimeout(() => {
+      const isFetchingData = this.select(this.flux.selectors.isFetching);
+      const isFetching = Object.keys(isFetchingData).some((key) => isFetchingData[key]);
+
+      if (isFetching) {
+        clearTimeout(timeout);
+        initTimeout();
+      } else {
+        this.flux.emit(Events.TAG_SETTLED);
+      }
+    }, this.config.options.settledTimeout);
+  };
+
   PUSH_PHASES.forEach((phase) => this.on(phase, () => {
     clearTimeout(timeout);
     ++inProgress;
     console.log('DEBUG in progress', phase, inProgress, this.root.tagName);
   }));
   POP_PHASES.forEach((phase) => this.on(phase, () => {
-    if (!--inProgress) timeout = setTimeout(() => this.flux.emit(Events.TAG_SETTLED), this.config.options.settledTimeout);
+    if (!--inProgress) initTimeout();
     console.log('DEBUG in progress', phase, inProgress, this.root.tagName);
   }));
 }
